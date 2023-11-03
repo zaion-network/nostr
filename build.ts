331a66516357e49a60b9202f855e57ddb33232d4
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-11-03 10:27:45 +0100

    add build script

--- build.ts
+++ build.ts
@@ -3,7 +3,19 @@
 const res = await Bun.build({
   entrypoints: ["./src/index.ts", "./web/index.ts"],
   outdir: "dist",
-  target: "bun"
+  target: "bun",
+});
+const res2 = await Bun.build({
+  entrypoints: ["./server/index.ts"],
+  outdir: "dist/server",
+  target: "node",
+});
+const res3 = await Bun.build({
+  entrypoints: ["./server/noble/index.ts"],
+  outdir: "dist/server/noble",
+  target: "node",
 });
 
 console.log(res);
+console.log(res2);
+console.log(res3);
